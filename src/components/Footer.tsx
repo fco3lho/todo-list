9bf11c25470796fea15c4dc5039379c96aeb54c2
Felipe Campos <felipecampos50123@gmail.com>
2023-02-28 10:24:34 -0300

    Criando header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>To do list @ {new Date().getFullYear()}</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
